b23c3c80026c398f3209f15c712a43b4fafaa2ac
somethingsomethingtwice <167238930+somethingsomethingtwice@users.noreply.github.com>
2024-10-17 13:11:40 -0400

    Highscore screen

---
+++ HighscoreScreen.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import greenfoot.*;
+
+public class HighscoreScreen extends Screen {
+  private HashMap<String, Integer> map = new HashMap<String, Integer>();
+  private Label label = new Label("", 32);
+
+  public HighscoreScreen() {
+    super();
+
+    // Add to map
+    map.put("Ethan", 1);
+    map.put("Tom", 1000);
+    map.put("Sally", 2400);
+
+    int lines = 0;
+    String str = "";
+    for (Map.Entry<String, Integer> e : map.entrySet()) {
+      str += e.getKey() + ": " + e.getValue() + "\n";
+      lines++;
+    }
+    label.setValue(str);
+    addObject(label, 300, 64 + 16 * lines);
+    addObject(new Button(this::goBack, "Go to Menu", 144), 300, 340);
+  }
+
+  public void goBack() {
+    pop();
+  }
+}
--- MenuScreen.java
+++ MenuScreen.java
@@ -8,11 +8,16 @@ public class MenuScreen extends Screen {
     push();
 
     addObject(avatar, 300 - 96, 160);
-    addObject(new Button(this::goInstructions, "Instructions", 160), 120, 340);
+    addObject(new Button(this::goInstructions, "Instructions", 160), 300 - 96, 340);
+    addObject(new Button(this::goHighscores, "Highscores", 160), 300 + 96, 340);
     addObject(new Button(avatar::nextAvatar, "Next"), 300 + 96, 160);
   }
 
   public void goInstructions() {
     new InstructionScreen().push();
+  }
+
+  public void goHighscores() {
+    new HighscoreScreen().push();
   }
 }
